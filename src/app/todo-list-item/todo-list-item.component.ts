fcd4d1330ce1032efa53503498739fbe6573c930
antoinebouard <57663747+antoinebouard@users.noreply.github.com>
2021-04-29 11:18:46 +0200

    Update todo-list-item.component.ts

--- src/app/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list-item/todo-list-item.component.ts
@@ -8,7 +8,7 @@ export interface Todo {
 @Component({
   selector: 'app-todo-list-item',
   template: `
-  <p>ID : {{ text.id }} - {{ text.value }}</p><button (click)="tabText.splice(tabText.indexOf(text), 1)">Supprimer</button>
+  <p>ID : {{ text.id }} - {{ text.value }}</p><button (click)="delete.emit()">Supprimer</button>
   `,
   styles: [``]
 })
@@ -16,6 +16,7 @@ export class TodoListItemComponent implements OnInit {
 
   @Input() text: Todo;
   @Input() tabText: Todo[]
+  @Output() delete = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
